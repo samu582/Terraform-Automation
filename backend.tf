781fe06b5e07c5cfa5e253a962bbaf1e8e3e8b54
samu582 <samupisolkar582@gmail.com>
2025-04-06 14:41:58 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "mydev-project-terraform-sample-batch27-2025"
     key = "main"
     region = "us-east-1"
-    dynamodb_table = "my-dynamodb-table2"
+    dynamodb_table = "my-dynamodb-table"
   }
 }
